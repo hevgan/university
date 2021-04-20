07322546dd47050315c3a80c273a78fd1f23685e
Maciej Adryan <79442250+hevgan@users.noreply.github.com>
2021-04-20 19:57:03 +0200

    merged branches
    * added partial solution for ex1
    * added plotting, plot saving and partial solution to ex2

--- MN/lab4/zad_1.m
+++ MN/lab4/zad_1.m
@@ -1,44 +1,115 @@
 clear 
 clc
-
+Index = 175854;
+N0 = mod(Index,10);
+N1 = mod(N0-1,4)+1; %N1 = 4
 %ex1
-eps = 10^-3;
-l = 1;
-r = 60000;
+params = [1,60000,10^-3];
 
-[bisec_xvect,   bisec_xdif,     bisec_fx,   bisec_it_cnt]   = bisectionMethod(l,r,eps,@function1_1);
-[secant_xvect2, secant_xdif2,   secant_fx2, secant_it_cnt2] = secantMethod(l,r,eps,@function1_1);
+[bisec_xvect,   bisec_xdif,     bisec_fx,   bisec_it_cnt]   = bisectionMethod(params,@function1_1);
+[secant_xvect2, secant_xdif2,   secant_fx2, secant_it_cnt2] = secantMethod(params,@function1_1);
 
-fprintf("bisectionMethod %d iterations\n", bisec_it_cnt);
-fprintf("secantMethod %d iterations\n", secant_it_cnt2);
+%fprintf("bisectionMethod %d iterations\n", bisec_it_cnt);
+%fprintf("secantMethod %d iterations\n", secant_it_cnt2);
+fprintf("max parameter count ≈ %f \n", (bisec_xvect(end)));
+plot(bisec_xdif);
+exportgraphics(gcf, 'plots\1.png')
+plot(secant_xdif2);
+exportgraphics(gcf, 'plots\2.png')
+
+plot(bisec_fx);
+exportgraphics(gcf, 'plots\3.png')
+
+plot(secant_fx2);
+exportgraphics(gcf, 'plots\4.png')
 
 %ex2
-expected_result = 75;
-R = 725;
-C = 8*10^-5;
-L = 2;
-l = 0;
-r = 50;
-eps = 10^-12;
+params = [1,50,10^-12];
 
-%[a,b,c,d] =  bisectionMethod(l,r,eps,@function1_2);
-%[e,f,g,h] =  secantMethod(l,r,eps,@function1_2);
+%rename the variables
+[a,b,c,d] =  bisectionMethod(params,@function1_2);
+[e,f,g,h] =  secantMethod(params,@function1_2);
+%fprintf("bisectionMethod %d iterations\n", d);
+%fprintf("secantMethod %d iterations\n", h);
+fprintf("angular frequency ≈ %f[rad/s] \n", (a(end)));
 
-function result=function1_1(N)
+
+plot(b);
+exportgraphics(gcf, 'plots\5.png')
+
+plot(f);
+exportgraphics(gcf, 'plots\6.png')
+
+plot(c);
+exportgraphics(gcf, 'plots\7.png')
+plot(g);
+exportgraphics(gcf, 'plots\8.png')
+
+
+
+%ex3
+params = [0,50,10^-12];
+
+[i,j,k,l] =  bisectionMethod(params,@function1_3);
+[m,n,o,p] =  secantMethod(params,@function1_3);
+%fprintf("bisectionMethod %d iterations\n", l);
+%fprintf("secantMethod %d iterations\n", p);
+fprintf("time to reach 750m/s ≈ %f[s] \n", (i(end)));
+
+
+
+
+plot(j);
+exportgraphics(gcf, 'plots\9.png')
+
+exportgraphics(gcf, 'plots\10.png')
+plot(n);
+
+exportgraphics(gcf, 'plots\11.png')
+plot(k);
+exportgraphics(gcf, 'plots\12.png')
+plot(o);
+
+
+
+function result=function1_1(params)
     
-    result = (N^1.43+N^1.14)/1000 -5000;
+    treshold = 5000;
+    N = params(1);
+    result = (N^1.43+N^1.14)/1000 -treshold;
 
 end
 
-function result=function1_2(omega, R, C, L )
-    
-    denominator = ((1/R^2)+(omega*C - 1/(omega*L))^2)^1/2;
-    result =  1/denominator-725;
+function result=function1_2(params)
+
+    expected_result = 75;
+    omega = params(1);
+    R = 725;
+    C = 8*10^-5;
+    L = 2;
+    denominator = ((1/R^2)+(omega*C - 1/(omega*L))^2)^(1/2);
+    result =  1/denominator-expected_result;
 
 end
 
-function [xvect,xdif,fx,it_cnt]=bisectionMethod(l,r,eps,func)
+function result=function1_3(params)
+    
+    g = 3.7;
+    v = 750;
+    m0 = 150000;
+    q = 2700;
+    u = 2000;
+    t = params(1);
+    ins_log = m0/(m0-q*t);
+    result = u*log(ins_log)-g*t-v;
 
+end
+
+
+function [xvect,xdif,fx,it_cnt]=bisectionMethod(params,func)
+    l = params(1);
+    r = params(2);
+    eps = params(3);
     xvect = [];
     xdif = [];
     fx = [];
@@ -64,8 +135,10 @@ function [xvect,xdif,fx,it_cnt]=bisectionMethod(l,r,eps,func)
     
 end
 
-function [xvect,xdif,fx,it_cnt]=secantMethod(l,r,eps,func)
-
+function [xvect,xdif,fx,it_cnt]=secantMethod(params,func)
+    l = params(1);
+    r = params(2);
+    eps = params(3);
     xvect = [l;r];
     fx = [feval(func,l); feval(func,r)];
     xdif = [];
